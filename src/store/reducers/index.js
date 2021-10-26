7e62ff7d659d5efe9143c3ac7e05b2f2f602b4fe
sohagmahin <sohagmahin@gmail.com>
2021-10-26 11:16:22 +0600

    add global error modal

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -4,8 +4,13 @@ import { combineReducers } from 'redux';
 // internal imports
 import authReducer from './auth';
 
+// error modal reducer
+import errorModalReducer from './errorModal/errorModal';
+
+
 const reducers = combineReducers({
     auth: authReducer,
+    errorModal: errorModalReducer,
 });
 
 export default reducers;
